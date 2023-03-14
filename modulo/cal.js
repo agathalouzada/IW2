b6ef9db2e7dd62d0e6725669ddb1128a743784e3
Ágatha Louzada <100931506+agathalouzada@users.noreply.github.com>
2023-03-14 12:33:04 -0300

    Add files via upload

---
+++ modulo/cal.js
@@ -0,0 +1,10 @@
+const somaF = require('./soma')
+const subF = require('./sub')
+const divF = require('./div')
+const multF = require('./mult')
+
+
+console.log("A soma é: " + somaF(3,3))
+console.log("A subtracao é: " + subF(3,3))
+console.log("A divisao é: " + divF(3,3))
+console.log("A multiplicacao é: " + multF(3,3))
